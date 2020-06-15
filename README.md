9377d5895b4bb93468214e65357254df5bd444a8
luxingyuan_root <luxingyuan_root@163.com>
2020-06-15 15:13:31 +0800

    seata1.2
    seata1.2

--- README.md
+++
@@ -1,5 +0,0 @@
-# spring-cloud-demo
-spring-cloud-demo
-
-
-
--- application-common/application-dao/src/main/java/com/example/order/mapper/ARocketmqOrderInfoMapper.java
+++
@@ -1,7 +0,0 @@
-package com.example.order.mapper;
-
-import com.example.order.domain.ARocketmqOrderInfo;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface ARocketmqOrderInfoMapper extends Mapper<ARocketmqOrderInfo> {
-}
